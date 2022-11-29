3e20a6c668105ef884435e6df81d57cebdbd4f9e
kyle-woodward <kw.geospatial@gmail.com>
2022-11-29 12:01:51 -0500

    add --rasterize_on flag for flexibility

--- src/CreateDistLayer/rasterize_treatments_ee_custom.py
+++ src/CreateDistLayer/rasterize_treatments_ee_custom.py
@@ -19,15 +19,50 @@ try:
 except:
     ee.Initialize()
 
+def ranks_to_dist(img:ee.Image):
+    codes_list = ee.List([131, 132, 133,    # fire high sev | most recent to least                      
+                        331, 332, 333,    # mech remove high sev | most recent to least               
+                        121, 122, 123,    # all other                                          
+                        111, 112, 113,    #   fires                            
+                        321, 231, 831,    #                                                       
+                        311, 221, 821,    #                                                          
+                        322, 211, 811,    #        all other dists ranked by                               
+                        312, 232, 832,    #             TSD, type, sev                                                         
+                        323, 222, 822,    #  type rank goes mech remove, mech add, other                                               
+                        313, 212, 812,                                                                
+                            233, 833,    
+                            223, 823,    
+                            213, 813])             
+
+    ranks_list = ee.List([36, 35, 34,
+                    33, 32, 31,
+                    30, 29, 28,
+                    27, 26, 25,
+                    24, 23, 22, 
+                    21, 20, 19, 
+                    18, 17, 16,
+                    15, 14, 13,
+                    12, 11, 10, 
+                    9, 8, 7, 
+                    6, 5, 4, 
+                    3, 2, 1])
+
+    # treatments DIST ranks asset
+    treatments_ranks = ee.Image(img)
+    
+    # remap back to DIST codes
+    dist_img = treatments_ranks.remap(ranks_list, codes_list, 0).rename('DIST')
+    return dist_img
+
 # Potential different avenue to the curernt rasterize_treatments.py - rasterize each treatments_ranks FC on ranks property into one ee.Image
 # testing if it works and is faster than local processing route
 # Load in treatment FCs with ranks property
 def main():
-    """Main level function for generating new CBH and CBD"""
+    """rasterize dist FC on ranks or DIST property"""
     
     # initalize new cli parser
     parser = argparse.ArgumentParser(
-        description="CLI process for generating new CBH and CBD."
+        description="rasterize dist FC on ranks or DIST property"
     )
 
     parser.add_argument(
@@ -48,6 +83,13 @@ def main():
         "--output",
         type=str,
         help="Earth Engine Asset output asset path"
+    )
+
+    parser.add_argument(
+        "-r",
+        "--rasterize_on",
+        type=str,
+        help="property to rasterize on, one of DIST or ranks"
     )
     args = parser.parse_args()
 
@@ -72,13 +114,20 @@ def main():
     
     dist_w_ranks = ee.FeatureCollection(args.input) # plug EE asset path straight in insted of version-based file naming
     ranksImg = dist_w_ranks.reduceToImage(['ranks'], ee.Reducer.max()).rename('ranks')
-
-    # logger.info(ranksImg.bandNames().getInfo())
+    
+    # rasterizes on ranks or DIST field given user input to --rasterize_on
+    if args.rasterize_on == "ranks":
+        final = ranksImg
+    elif args.rasterize_on == "DIST":
+        final = ranks_to_dist(ranksImg)
+    else:
+        raise ValueError(f"{args.rasterize_on} is not a valid property to rasterize on. Valid properties: ranks, DIST ")
+    
+    logger.info(final.bandNames().getInfo())
    
-    desc = os.path.basename(args.output).replace('/','_')
-        
+    desc = os.path.basename(args.output).replace('/','_') 
     task = ee.batch.Export.image.toAsset(
-        image=ranksImg,
+        image=final,
         description=desc,
         assetId=args.output,
         region=cc_img.geometry(),
@@ -87,7 +136,7 @@ def main():
         maxPixels=1e12,
     )
     task.start()  # kick off export
-    print(f'export task started: {desc}')
+    logger.info(f'export task started: {desc}')
 
 if __name__ =="__main__":
     main()
